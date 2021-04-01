b967492fcf4f07438fd9a063aceabaedd34504c3
maulana-alamsyah <maulanalamsyah@gmail.com>
2021-04-01 22:24:50 +0700

    Tugas 3: Algoritma Pencarian

---
+++ src/searching/binary.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package searching;
+import java.util.Scanner;
+public class binary {
+    public static void main(String[] args) {
+        int data[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        int posisiAwal = 0;
+        int posisiAkhir = data.length - 1;
+        int pointer;
+        boolean ketemu = false;
+        
+        
+        
+        System.out.println("***** Binary Search *****");
+        System.out.println();
+        
+        
+        
+        System.out.println("Isi data adalah");
+        for(int i=0; i<data.length; i++){
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+        
+        
+        
+        System.out.print("Masukkan data yang dicari : ");
+        Scanner masukan = new Scanner(System.in);
+        int cari = masukan.nextInt();
+        
+        
+        
+        while((posisiAwal <= posisiAkhir) && (ketemu == false)){
+            pointer = (posisiAwal + posisiAkhir) / 2;
+            
+            System.out.println("index pointer : " + pointer);
+            if(cari == data[pointer]){
+                ketemu = true;
+                System.out.println("Data " + cari + " telah ditemukan pada index ke " + pointer);
+            }else if(cari < data[pointer] ){
+                System.out.println("cari di kiri");
+                posisiAkhir = pointer - 1;
+            }else if(cari > data[pointer]){
+                System.out.println("cari di kanan");
+                posisiAwal = pointer + 1;
+            }
+        }
+        
+        
+        
+        if(ketemu){
+            System.out.println("Kesimpulan: data ditemukan");
+        }else{
+            System.out.println("Kesimpulan: data tidak ditemukan");
+        }
+        
+        
+        
+        System.out.println();
+        System.out.println("    Maulana Alamsyah");
+        System.out.println("***** Terimakasih *****");
+    }
+}
---
+++ src/searching/sequential.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package searching;
+import java.util.Scanner;
+public class sequential {
+    public static void main(String[] args) {
+        String data[] = {"Galileo", "Archimedes", "Alkhawarizmi", "Aljabar", "Nama Mahasiswa", "Tesla"};
+        boolean ketemu = false;
+        
+        
+        
+        System.out.println("***** Secuential Search *****");
+        System.out.println();
+        
+        
+        
+        System.out.print("Isi data adalah: | ");
+        for(int i=0; i < data.length; i++){
+            System.out.print(data[i] + " | ");
+        }
+        System.out.println();
+        
+        
+        
+        System.out.print("Masukkan data yang dicari: ");
+        Scanner masukan = new Scanner(System.in);
+        String cari = masukan.nextLine();
+        
+        
+        
+        for(int i=0; i<data.length; i++){
+            if(cari.equalsIgnoreCase(data[i])){
+                System.out.println("Data " + cari + " berada pada index ke-" + i);
+                ketemu = true;
+                break;
+            }
+        }
+        
+        
+        
+        if(!ketemu){
+            System.out.println("data tidak ditemukan");
+        }
+        
+        
+        
+        System.out.println();
+        System.out.println("    Maulana Alamsyah");
+        System.out.println("***** Terimakasih *****");
+    }
+}
